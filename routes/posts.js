de1eea9ba6642c4eba9950d1b6aafeaa045a6d96
Permadi Wibisono <mbapewe@gmail.com>
2018-09-18 02:54:52 +0700

    Validation Request using Joi

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 
 const Post = require('../models/posts.js');
 
@@ -11,6 +12,9 @@ router.get('/',(req, res)=>{
 });
 
 router.post('/',(req,res)=>{
+  const { error } = validateRequest(req);
+  if(error) return res.status(422).json(getValidationError(error));
+
   const newPost = new Post(Object.assign({},req.body));
   newPost.save((err)=>{
     if(err) return res.status(500).send(err);
@@ -26,6 +30,8 @@ router.get('/:id',(req,res)=>{
 })
 
 router.put('/:id',(req,res)=>{
+  if(error) return res.status(422).json(getValidationError(error));
+
   Post.findByIdAndUpdate(req.params.id,Object.assign({updated_at:Date.now()},req.body),{new:true},(err, post)=>{
     if(err) return res.status(500).send(err);
     return res.json({message:'Post updated!', data: post, status_code:200});
@@ -38,4 +44,24 @@ router.delete('/:id',(req,res)=>{
     return res.json({message:'Post deleted!', status_code:200});
   });
 });
+
+const validateRequest = ({ body }) => {
+  const schema = {
+    body: Joi.string().required(),
+    slug: Joi.string().required(),
+    title: Joi.string().required(),
+    hidden: Joi.boolean(),
+    featured: Joi.boolean()
+  };
+  return Joi.validate(body, schema);
+}
+
+const getValidationError = ({ details }) => ({
+  error: {
+    errors: details.map(err => ({ label: err.context.label, message: err.message })),
+    message: "Validation Error",
+    status_code: 422
+  }
+});
+
 module.exports = router;
