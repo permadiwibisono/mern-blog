cfb5367fd9f8eec7dfced7027e7294d7481fa854
Permadi Wibisono <mbapewe@gmail.com>
2018-09-18 02:21:11 +0700

    Refactor routes/posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,42 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var Post = require('../models/posts.js');
+const Post = require('../models/posts.js');
+
 router.get('/',(req, res)=>{
-    Post.find((err, posts)=>{
-        if(err)
-            res.send(err);
-        res.json({data:posts, message:"Get list posts", status_code:200});
-    })
+  Post.find((err, posts)=>{
+    if(err) return res.status(500).send(err);
+    return res.json({data:posts, message:"Get list posts", status_code:200});
+  });
 });
-router.post('/',(req,res)=>{
-    var newPost = new Post(Object.assign({},req.body));
 
-    newPost.save((err)=>{
-        if(err)
-            res.send(err);
-        res.json({message:'Post created!', status_code:200});
-    })
+router.post('/',(req,res)=>{
+  const newPost = new Post(Object.assign({},req.body));
+  newPost.save((err)=>{
+    if(err) return res.status(500).send(err);
+    return res.json({message:'Post created!', status_code:200});
+  });
+});
+
+router.get('/:id',(req,res)=>{
+  Post.findById(req.params.id,(err, post)=>{
+    if(err) return res.status(500).send(err);
+    return res.json({message:'Get a post!', data: post, status_code:200});
+  })
 })
-router.get('/:post_id',(req,res)=>{
-    Post.findById(req.params.post_id,(err, post)=>{
-        if(err)
-            res.send(err);
-        res.json({message:'Get a post!', data: post, status_code:200});
-    })
-})
-router.put('/:post_id',(req,res)=>{
-    Post.findByIdAndUpdate(req.params.post_id,Object.assign({},req.body),{new:true},(err, post)=>{
-        if(err)
-            res.send(err);
-        res.json({message:'Post updated!', data: post, status_code:200});
-    })
-})
-router.delete('/:post_id',(req,res)=>{
-    Post.findByIdAndRemove(req.params.post_id,(err)=>{
-        if(err)
-            res.send(err);
-        res.json({message:'Post deleted!', status_code:200});
-    })
-})
+
+router.put('/:id',(req,res)=>{
+  Post.findByIdAndUpdate(req.params.id,Object.assign({updated_at:Date.now()},req.body),{new:true},(err, post)=>{
+    if(err) return res.status(500).send(err);
+    return res.json({message:'Post updated!', data: post, status_code:200});
+  });
+});
+
+router.delete('/:id',(req,res)=>{
+  Post.findByIdAndRemove(req.params.id,(err)=>{
+    if(err) return res.status(500).send(err);
+    return res.json({message:'Post deleted!', status_code:200});
+  });
+});
 module.exports = router;
